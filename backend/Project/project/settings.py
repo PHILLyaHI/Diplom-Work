975b6191987c44777f9fcc287515ab0d400e4385
PHILLyaHI <phillippapetenok@gmail.com>
2022-12-20 00:05:01 -0800

    webpush changes

--- backend/Project/project/settings.py
+++ backend/Project/project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-#from django_jinja.builtins import DEFAULT_EXTENSIONS
+from django_jinja.builtins import DEFAULT_EXTENSIONS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,32 +72,17 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'project.urls'
 
 
-# TEMPLATES = [
-#     {
-#         "BACKEND": "django_jinja.backend.Jinja2",
-#         "OPTIONS": {
-#             "extensions": DEFAULT_EXTENSIONS + [
-#                 "webpush.jinja2.WebPushExtension"
-#             ]
-#         }
-#     }
-# ]
-
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+        "BACKEND": "django_jinja.backend.Jinja2",
+        "OPTIONS": {
+            "extensions": DEFAULT_EXTENSIONS + [
+                "webpush.jinja2.WebPushExtension"
+            ]
+        }
+    }
 ]
+
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
